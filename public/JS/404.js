839d0a306f5dbaad0592688d331c22897810d35d
NekoFathari <nekofathari@gmail.com>
2023-06-14 12:36:47 +0700

    modified:   public/CSS/404.css 	modified:   public/JS/404.js

--- public/JS/404.js
+++ public/JS/404.js
@@ -1,6 +1,16 @@
-const element = document.querySelector('.-wraper');
-element.style.setProperty('--width', "100%");
+const loadingBar = document.querySelector('.-wraper');
+loadingBar.style.setProperty('--width', "100%");
 
 setTimeout(() => {
     window.location.href = window.history.back(1);
  }, 10000);
+
+ const oyen = document.querySelector(".oren").innerText;
+
+ const cd = () => {
+    setTimeout(() => {
+        oyen = "9";
+     }, 1000);
+ }
+
+ cd();
