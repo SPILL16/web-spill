133644f93bf11a17cefa6ea56a9a2f7e3fccbb01
ARNN5115 <andika.rizky.noval.n.5115@gmail.com>
2023-06-15 10:35:27 +0700

    modified:   public/JS/404.js

--- public/JS/404.js
+++ public/JS/404.js
@@ -2,14 +2,21 @@ const loadingBar = document.querySelector('.-wraper');
 loadingBar.style.setProperty('--width', "100%");
 
 setTimeout(() => {
-    window.location.href = window.history.back(1);
- }, 10000);
+    window.location.href = window.history.back();
+}, 10000);
 
- const oyen = document.querySelector(".oren").innerText;
+let oyen = document.querySelector(".oren");
 
- const cd = () => {
+let count = 9;
+
+const cd = () => {
     setTimeout(() => {
-        oyen = "9";
+        let countS = "" + count;
+        oyen.innerText = countS;
+        if (count > 0) {
+            count--;
+            cd();
+        }
      }, 1000);
  }
 
