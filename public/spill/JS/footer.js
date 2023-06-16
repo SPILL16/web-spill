429306e0946c76ee6b7b8ad8a4609db486cbec87
ARNN5115 <andika.rizky.noval.n.5115@gmail.com>
2023-06-16 11:09:17 +0700

    modified:   public/spill/CSS/about.css 	modified:   public/spill/CSS/footer.css 	modified:   public/spill/HTML/about.html 	modified:   public/spill/JS/footer.js

--- public/spill/JS/footer.js
+++ public/spill/JS/footer.js
@@ -13,7 +13,7 @@ class Footer extends HTMLElement{
         `
             <footer>
                 <div id="hubungi">
-                    <div class="container hubungi-sort">
+                    <div class="hubungi-sort">
                         <svg class="wave2 wave-debug-idk"
                             width="2655.4221"
                             height="313.12579"
@@ -34,7 +34,7 @@ class Footer extends HTMLElement{
                         </svg>
                         <h2 class="hubungi-h"><span class="hubungi-h-em">Gimana?</span> Tertarik?</h2>
                         <h3 class="hubungi-h-2">Yuk Hubungi Kami</h3>
-                        <div class="hubungi-p-wraper">
+                        <a class="hubungi-p-wraper"  href="https://wa.me/62895631408165">
                             <svg class="logo-wa" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                                 viewBox="0 0 308 308" style="enable-background:new 0 0 308 308;" xml:space="preserve">
                             <g id="XMLID_468_">
@@ -62,7 +62,7 @@ class Footer extends HTMLElement{
                                 <br/ >
                                 +62 895-6314-08165
                             </p>
-                        </div>
+                        </a>
                 </div>
 
                 <div id="copyr">
